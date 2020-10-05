29cba23a7dcb962e688145f16e728bcc26f6ef18
tiki7732 <lsaidah@gmail.com>
2020-10-05 15:01:18 -0500

    started the Question class, user class, and reply class

--- QuestionDatabase.rb
+++ QuestionDatabase.rb
@@ -1,7 +1,7 @@
 require 'singleton'
 require 'sqlite3'
 
-class QuestionDatabase << SQLite3::Database
+class QuestionDatabase < SQLite3::Database
     include Singleton
 
     def initialize  
@@ -11,6 +11,92 @@ class QuestionDatabase << SQLite3::Database
   end
 end
 
-class Questions
+class Question
 
+    attr_accessor :id, :title, :body, :author_id
+
+    def self.all
+        data = QuestionDatabase.instance.execute("SELECT * FROM questions")
+        data.map{|question| Question.new(question)}
+    end
+
+    def self.find_by_id(id)
+        question = QuestionDatabase.instance.execute(<<-SQL, id)
+        SELECT *
+        FROM questions
+        WHERE id = ?
+        SQL
+        return nil unless question.length > 0
+        Question.new(question.first)
+    end
+
+    def self.find_by_title(title)
+        question = QuestionDatabase.instance.execute(<<-SQL, title)
+        SELECT *
+        FROM questions
+        WHERE title = ?
+        SQL
+        return nil unless question.length > 0
+        Question.new(question.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
+end
+
+class User
+    attr_accessor :id, :fname, :lname
+
+    def self.all
+        data = QuestionDatabase.instance.execute("SELECT * FROM users")
+        data.map{|user| User.new(user)}
+    end
+
+    def self.find_by_id(id)
+        user = QuestionDatabase.instance.execute(<<-SQL, id)
+        SELECT *
+        FROM users
+        WHERE id = ?
+        SQL
+        return nil unless user.length > 0
+        User.new(user.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+end
+
+class Reply
+
+    attr_accessor :id, :question_id, :parent_reply_id, :author_id, :body
+
+    def self.all
+        data = QuestionDatabase.instance.execute("SELECT * FROM replies")
+        data.map{|reply| Reply.new(reply)}
+    end
+
+    def self.find_by_id(id)
+        reply = QuestionDatabase.instance.execute(<<-SQL, id)
+        SELECT *
+        FROM replies
+        WHERE id = ?
+        SQL
+        return nil unless reply.length > 0
+        Reply.new(reply.first)
+    end
+
+    def inititalize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @parent_reply_id = options['parent_reply_id']
+        @author_id = options['author_id']
+        @body = options['body']
+    end
 end
